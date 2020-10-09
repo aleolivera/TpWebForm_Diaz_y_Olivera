b166befaf25213b31987ab78f367b9ac8fce4584
Ailen Diaz <63076335+ailendiaz@users.noreply.github.com>
2020-10-09 12:20:09 -0300

    Agrege las referencias a Dominio y Negocio. Agrege el iterador y ya se pueden ver las imagenes y nombres de los articulos en el Home.

--- WebForm/Default.aspx.cs
+++ WebForm/Default.aspx.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace WebForm
 {
     public partial class _Default : Page
     {
+        public List<Articulos>listaarticulo { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ArticulosNegocio negocio = new ArticulosNegocio();
+            listaarticulo = negocio.listar();
         }
     }
 }
